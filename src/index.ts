4f369561564fb2aacdf770287a736f0828a08c41
root <root@Prahastha.localdomain>
2026-09-09 14:42:10 +0700

    Fix category to component, clean up template boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-export { ExampleTransformer } from "./transformer";
-export { ExampleFilter } from "./filter";
-export { ExampleEmitter } from "./emitter";
 export { default as RagChatWidget } from "./components/RagChatWidget";
-
-export type {
-  ExampleTransformerOptions,
-  ExampleFilterOptions,
-  ExampleEmitterOptions,
-} from "./types";
-
 export type { RagChatWidgetOptions } from "./components/RagChatWidget";
 
 // Re-export shared types from @quartz-community/types
@@ -16,13 +6,4 @@ export type {
   QuartzComponent,
   QuartzComponentProps,
   QuartzComponentConstructor,
-  StringResource,
-  QuartzTransformerPlugin,
-  QuartzFilterPlugin,
-  QuartzEmitterPlugin,
-  QuartzPageTypePlugin,
-  QuartzPageTypePluginInstance,
-  PageMatcher,
-  PageGenerator,
-  VirtualPage,
 } from "@quartz-community/types";
